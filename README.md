9d2429413558a52d4135522d1b1e4e39e1f5b212
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-09-20 01:05:26 +0530

    Added Header component and basic layout

--- README.md
+++ README.md
@@ -5,4 +5,9 @@
 **Initialize the React project**
 - Installed & configuring Tailwind CSS and react-icons setup.
 
-
+## Features - Header
+- Header with:
+  - Logo
+  - Title ("Tasks")
+  - Action buttons (New Task and Refresh)
+- Responsive design using Tailwind CSS
--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './index.css';
 
+import { FcRefresh } from "react-icons/fc";
+
+const Header = () => (
+  <header className="flex justify-between items-center p-4 border-2 bg-gray-100">
+    <div className="flex items-center h-[55px] overflow-hidden p-2">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUnk0Pdm1QoRzmOVTJdG4bCA0QzG0gxbhMFg&s" alt="Logo" className="h-full w-auto object-cover mr-2" />
+      <h1 className="text-xl font-semibold">Tasks</h1>
+    </div>
+    <div className="flex space-x-4">
+      <button>New Task</button>
+      <button>
+        <FcRefresh />
+      </button>
+    </div>
+  </header>
+);
+
 function App() {
+
   return (
     <>
-    <h1 className='text-red-500'>Hello react!!</h1>
+      <Header />
     </>
   );
 }
